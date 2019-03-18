9a53a3d3b9955959a75269b640439bc04d62b3d1
aymen <aymen.sellaouti@gmail.com>
2019-03-18 10:37:46 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
